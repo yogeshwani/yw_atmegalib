4ff262336ea60ef11f4cf959b5d4a2878f0cf2f4
Yogesh Wani <yogeshwani@gmail.com>
2016-10-07 16:49:51 +0100

    Cosmetic changes to make some indentation proper and make the comments a bit clearer.

--- ADC/adc.c
+++ ADC/adc.c
@@ -2,11 +2,13 @@
  * File : adc.c
  *
  * Description:
- * File contains the necessary functions for using the internal adc of the AVR microcontroller 
+ * File contains the necessary functions for using the internal adc of the
+ * AVR microcontroller
  *
- * Note: 
- * For detail documentation about the different functions and the use of ADC refer the header file adc.h 
- * Refer to E:/Avrprojects/library/coding standards.doc for coding standards
+ * Note:
+ * For detail documentation about the different functions and the use of ADC
+ * refer the header file adc.h
+ * Refer to <path>Avrprojects/library/coding standards.doc for coding standards
  *
  */
 #include <avr/io.h>
@@ -15,113 +17,117 @@
 
 /*
  * Function: AdcInit()
- * 
+ *
  * Description: Initializes the Adc module. For more details see adc.h
  *
- * Returns: Nothing 
+ * Returns: Nothing
  */
 
-/* implementation note :
-   Also things like the auto trigger and interrupt handling is still missing 
-   may be that can be implemented int he future 
-   An additional thing that needs implementing is the differential channel 
-   selection with adjustable gain 
-   for this implementation the positive and the negative channel number can be taken 
+/* Implementation note :
+   Also things like the auto trigger and interrupt handling is still missing
+   may be that can be implemented in the future
+   An additional thing that needs implementing is the differential channel
+   selection with adjustable gain
+   for this implementation the positive and the negative channel number can be
+   taken
    as input parameter along with the gain required !!
-*/ 
+*/
 void AdcInit(REFERENCE_VOLTAGE ref_volts, ADC_CLK_PRESCALE adc_prescale,
              bool left_justified, bool interrut_enabled/*,auto_trigger_enable*/)
 {
-     /* ADC Usage : */
-     /* Make the port of the adc as teh input ?? really is this needed ??*?*/
-	 
-	 /*Enable the Adc */
-	 EnableAdc();
-	
-	 /*Select the prescale (conversion speed)
-	   THe FCPU in a normal case is 8MHz/16MHz a conversion speed of 8MHZ by 128 
-	   is good enough.This pre-scaling factor is kept as default if nothing or 
-	   wrong prescale is provided which should not be the case as it needs an 
-	   enum which has to have a value which is defined.
+    /* ADC Usage : */
+    /* Make the port of the adc as the input ?? really is this needed ??*?*/
+
+    /* Enable the Adc */
+    EnableAdc();
+
+    /* Select the prescale (conversion speed)
+     * The FCPU in a normal case is 8MHz/16MHz a conversion speed of 8MHZ by 128
+     * is good enough.This pre-scaling factor is kept as default if nothing or
+     * wrong prescale is provided which should not be the case as it needs and
+     * enum which has to have a value which is defined.
      */
-	 switch(adc_prescale)
-     {
-		 case CLK_DIV_BY_2 :
-	         ADCSRA |= (1<<ADPS0);	 
-		 break;
-		 case CLK_DIV_BY_4 :
-		     ADCSRA |= (2<<ADPS0);
-		 break;
-		 case CLK_DIV_BY_8 :
-		     ADCSRA |= (3<<ADPS0);
-		 break;
-		 case CLK_DIV_BY_16 :
-		     ADCSRA |= (4<<ADPS0);
-		 break;
-         case CLK_DIV_BY_32 :
-		     ADCSRA |= (5<<ADPS0);
-		 break;
-         case CLK_DIV_BY_64 :
-		     ADCSRA |= (6<<ADPS0);
-		 break;
-		 case CLK_DIV_BY_128 :
-		     ADCSRA |= (7<<ADPS0);
-		 break;
-         default:
-		     ADCSRA |= (7<<ADPS0);
-		 break;  
-	 }	 
-	    
-	/*Select the reference voltage for the ADC 
-      Bits REFS1 and REFS0 in ADMUX reg 
-      Can be internal 2.56/ AVCC / or AREF (connected to external reference source)
-	  the default is the internal 2.56v 
-	  */
-	 switch (ref_volts)
-	 {
-		 case REF_VLTG_AREF :
-		     ADMUX |= (REF_VLTG_AREF<<REFS0);
-		 break;
-		 case REF_VLTG_AVCC :
-		     ADMUX |= (REF_VLTG_AVCC<<REFS0);
-		 break;
-		 case REF_VLTG_256_INTERNAL :
-		     ADMUX |= (REF_VLTG_256_INTERNAL<<REFS0);
-		 break;
-		 default:
-		     ADMUX |= (REF_VLTG_256_INTERNAL<<REFS0);
-		 break;
-		 
-	 }
-	/* Se the respective bit if left justified result is asked else right justify the result */
-	  if (left_justified)
-	      ADMUX |= (1<<ADLAR); // set the ADLAR bit 
-	  else 
-	      ADMUX &= ~(1<<ADLAR); // clear the ADLAR bit
+    switch(adc_prescale)
+    {
+        case CLK_DIV_BY_2 :
+            ADCSRA |= (1<<ADPS0);
+            break;
+        case CLK_DIV_BY_4 :
+            ADCSRA |= (2<<ADPS0);
+            break;
+        case CLK_DIV_BY_8 :
+            ADCSRA |= (3<<ADPS0);
+            break;
+        case CLK_DIV_BY_16 :
+            ADCSRA |= (4<<ADPS0);
+            break;
+        case CLK_DIV_BY_32 :
+            ADCSRA |= (5<<ADPS0);
+            break;
+        case CLK_DIV_BY_64 :
+            ADCSRA |= (6<<ADPS0);
+            break;
+        case CLK_DIV_BY_128 :
+            ADCSRA |= (7<<ADPS0);
+            break;
+        default:
+            ADCSRA |= (7<<ADPS0);
+            break;
+     }
+
+    /* Select the reference voltage for the ADC
+     * Bits REFS1 and REFS0 in ADMUX reg
+     * Can be internal 2.56/AVCC or AREF (connected to external reference source)
+     * The default is the internal 2.56v
+     */
+    switch (ref_volts)
+    {
+        case REF_VLTG_AREF :
+            ADMUX |= (REF_VLTG_AREF<<REFS0);
+            break;
+        case REF_VLTG_AVCC :
+            ADMUX |= (REF_VLTG_AVCC<<REFS0);
+            break;
+        case REF_VLTG_256_INTERNAL :
+            ADMUX |= (REF_VLTG_256_INTERNAL<<REFS0);
+            break;
+        default:
+            ADMUX |= (REF_VLTG_256_INTERNAL<<REFS0);
+            break;
+    }
+
+    /* Set the respective bit if left justified result is required else
+     * right justify the result
+     */
+    if (left_justified)
+        ADMUX |= (1<<ADLAR); // set the ADLAR bit
+    else
+        ADMUX &= ~(1<<ADLAR); // clear the ADLAR bit
 }
 
-uint16_t ReadAdc(uint8_t channel,bool shift_by_6_to_right)
+uint16_t ReadAdc(uint8_t channel, bool shift_by_6_to_right)
 {
-	 uint16_t adc_value;
-	
-	channel = (channel & MAX_NO_OF_ADC_CHANNELS);
-    /* Masthe higher nibble in order to prevent it form getting overwritten and clear the channel 
-    in order to write the new one */
-    ADMUX &= 0xE0; 
+    uint16_t adc_value;
+
+    channel = (channel & MAX_NO_OF_ADC_CHANNELS);
+    /* Mask the higher nibble in order to prevent it from getting overwritten
+     * and clear the channel in order to write the new one
+     */
+    ADMUX &= 0xE0;
     SetChannel(channel);
-	
-	/*Start the conversion and wait until ADIF is high (or interrupts can be used)*/
-	StartAdcConversion();
+
+    /*Start the conversion and wait until ADIF is high (else use interrupts)*/
+    StartAdcConversion();
     while(!(ADCSRA & (1<<ADIF)));
-	
-	/* Get data from the ADCH and ADCL register
-	 * This can be directly accessed by using ADC 
-	 */
-	adc_value = ADC;
-	
-	/* Write one back to the ADIF bit to clear it */
-	AdcReset();
-	if(!shift_by_6_to_right)
+
+    /* Get data from the ADCH and ADCL register
+     * This can be directly accessed by using ADC
+     */
+    adc_value = ADC;
+
+    /* Write one back to the ADIF bit to clear it */
+    AdcReset();
+    if(!shift_by_6_to_right)
     {
         return(adc_value);
     }
@@ -133,6 +139,6 @@ uint16_t ReadAdc(uint8_t channel,bool shift_by_6_to_right)
 
 void DisableInternalADC()
 {
-    /* Clearing the ADC Enable bit would disable the adc */	
+    /* Clearing the ADC Enable bit would disable the adc */
     ADCSRA &= ~(1<<ADEN);
 }
--- ADC/adc.h
+++ ADC/adc.h
@@ -1,57 +1,57 @@
 /**
  * @file adc.h
- * 
+ *
  * @brief Header file for the adc.c file
- *  
- * Contains the pin configurations, enums macros and function definitions for the adc   
+ *
+ * Contains the pin configurations, enums macros and function definitions for the adc
  *
  *
- *   Important things to know about the adc 
+ *   Important things to know about the adc
  *   REGISTERS :
- *    ADMUX   
+ *    ADMUX
  * ------------------------------------------------------------
- * | REFS1 | REFSO | ADLAR | MUX4 | MUX3 | MUX2 | MUX1 | MUXO | 
+ * | REFS1 | REFSO | ADLAR | MUX4 | MUX3 | MUX2 | MUX1 | MUXO |
  * ------------------------------------------------------------
  * REFSl:0 Bit 7:6 Reference Selection Bits
  *               These bits select the reference voltage for the ADC.
- *	
+ *
  * ADLAR Bit 5 ADC Left Adjust Results
- *				This bit dictates either the left bits or the right bits of the result registers ADCH:ADCL
-				that are used to store the result. If we write a one to ADLAR, the result will be left
-				adjusted; otherwise, the result is right adjusted.
-	MUX4:0 Bit 4:0 Analog Channel and Gain Selection Bits
-				   The value of these bits selects the gain for the differential
-				   channels and also selects which combination of analog inputs
-				   are connected to the ADC.
-				   
- ADCH and ADCL registers 
-               These registers store the 10 bit digital value after the conversion from the ADC 
-			   (ADLAR would decide the left/right adjustment. The upper or lower 6 bits out of 16 
-			    are unused and hence ADLAR would decide which ones be unused )
-
-ADCSRA 	
---------------------------------------------------------------			
+ * This bit dictates either the left bits or the right bits of the result registers ADCH:ADCL
+ *that are used to store the result. If we write a one to ADLAR, the result will be left
+ *adjusted; otherwise, the result is right adjusted.
+ *
+ *MUX4:0 Bit 4:0 Analog Channel and Gain Selection Bits
+ *The value of these bits selects the gain for the differential channels and also
+ *selects which combination of analog inputs are connected to the ADC.
+ *
+ * ADCH and ADCL registers
+ *             These registers store the 10 bit digital value after the conversion from the ADC
+ *                         (ADLAR would decide the left/right adjustment. The upper or lower 6 bits out of 16
+ *                          are unused and hence ADLAR would decide which ones be unused )
+ *
+ * ADCSRA
+--------------------------------------------------------------
 | ADEN | ADSC |  ADATE | ADIF | ADIE | ADPS2 | ADPS1 | ADPSO |
 --------------------------------------------------------------
 ADEN Bit 7 ADC Enable
-	This bit enables or disables the ADC. Setting this bit to one will enable the ADC, and
-	clearing this bit to zero will disable it even while a conversion is in progress.
+        This bit enables or disables the ADC. Setting this bit to one will enable the ADC, and
+        clearing this bit to zero will disable it even while a conversion is in progress.
 
-ADSC Bit 6 ADC Start Conversion
-	To start each conversion you have to set this bit to one.
+        ADSC Bit 6 ADC Start Conversion
+        To start each conversion you have to set this bit to one.
 
 ADATE Bit 5 ADC Auto Trigger Enable
-	Auto triggering of the ADC is enabled when you set this bit to one.
+        Auto triggering of the ADC is enabled when you set this bit to one.
 
 ADIF Bit 4 ADC Interrupt Flag
-	This bit is set when an ADC conversion completes and the data registers are updated.
+        This bit is set when an ADC conversion completes and the data registers are updated.
 
 ADIE Bit 3 ADC Interrupt Enable
-	Setting this bit to one enables the ADC conversion complete interrupt.
+        Setting this bit to one enables the ADC conversion complete interrupt.
 
 ADPS2:0 Bit 2:0 ADC Prescaler Select Bits
-	These bits determine the division factor between the XTAL frequency and the input
-	clock to the ADC.
+        These bits determine the division factor between the XTAL frequency and the input
+        clock to the ADC.
 */
 
 
@@ -63,13 +63,13 @@ ADPS2:0 Bit 2:0 ADC Prescaler Select Bits
 
 typedef enum adc_clk_prescale
 {
-	CLK_DIV_BY_2, 
-    CLK_DIV_BY_4, 
+    CLK_DIV_BY_2,
+    CLK_DIV_BY_4,
     CLK_DIV_BY_8,
-	CLK_DIV_BY_16,
-	CLK_DIV_BY_32, 
+    CLK_DIV_BY_16,
+    CLK_DIV_BY_32,
     CLK_DIV_BY_64,
-	CLK_DIV_BY_128
+    CLK_DIV_BY_128
 }ADC_CLK_PRESCALE;
 
 
@@ -88,30 +88,34 @@ typedef enum reference_voltage
 #define SetChannel(channel) ADMUX |= (channel);
 #define AdcReset() ADCSRA |= (1<<ADIF);
 
-/* USage for using the adc either in polling or using interrupt 
-   TBE implement and test properly */
+/* Usage for using the adc either in polling or using interrupt
+   TBD implement and test properly */
 
 #if USE_ADC_IN_INTERRUPT_MODE
     #define AdcEnableInterrupt() ADCSRA |= (1<<ADIE);
 #endif
 
-/** Initialize the ADC 
+/** Initialize the ADC
  * setups the adc based ont eh values provided as parameters to this function
  */
 void AdcInit(REFERENCE_VOLTAGE ref_volts, ADC_CLK_PRESCALE adc_prescale,
              bool left_justified, bool interrut_enabled/*,auto_trigger_enable*/);
 
 
-/** @brief  Read the value after the conversion from the ADC 
- * 
+/** @brief  Read the value after the conversion from the ADC
+ *
  * Takes the channel from which to read (0-7), the value passed in as bit wise
- * anded with the max no of channel to make sure it doesnt exceed the no of 
- * channels 
- * @param channel The channel number of the ADC to read from 
- * @param true_value The true_value of teh adc read rather than the 8 bit value 
+ * anded with the max no of channel to make sure it doesnt exceed the no of
+ * channels
+ * @param channel The channel number of the ADC to read from
+ * @param true_value The true_value of teh adc read rather than the 8 bit value
  * @return uint16_t (16bit) value from the analog to digital conversion
  */
 uint16_t ReadAdc(uint8_t channel, bool true_value);
 
-#endif 
+#endif
 
+
+/* this is a comemnt which his a kajslkdj  lkasdlaksh  lisahlahd ;
+ * kash lhaks lkashd llkahd ald lksh  lasjlakskjds d ljkasjd asdj lkj
+ * jksdjaksldja djk jaslkdj 
